30cf092526cc4513c6ffb71c2b55400babdabc8e
Morrie Cunningham <cunningm@mail.gvsu.edu>
2023-05-04 15:57:43 -0400

    Initial Commit.

---
+++ BizStreamFullStackAssignment/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using BizStreamFullStackAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System;
+using System.IO;
+using System.Text;
+
+namespace BizStreamFullStackAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SaveContactForm(ContactForm cf)
+        {
+            if (ModelState.IsValid)
+            {
+                string filename = "contactFormInfo_" + DateTime.Now.ToString("MMddyyyy_HHmmss") + ".txt";
+                string[] contents = { "First Name: " + cf.FName, "Last Name: " + cf.LName, "Email: " + cf.Email, "Message: " + cf.Message };
+                System.IO.File.WriteAllLines(AppDomain.CurrentDomain.BaseDirectory + filename, contents);
+                return RedirectToAction("Success");
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BizStreamFullStackAssignment/Models/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BizStreamFullStackAssignment.Models
+{
+    public partial class ContactForm
+    {
+        [DisplayName("First Name")]
+        public string? FName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string? LName { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Email is required.")]
+        public string? Email { get; set; }
+
+        [DisplayName("Message")]
+        [Column(TypeName = "text")]
+        [DataType(DataType.MultilineText)]
+        public string? Message { get; set; }
+    }
+}
